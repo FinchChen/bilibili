5ff2423fc5b1a3e92b9908fd5d4df1fca4df0533
Y CHEN <32098774+GlgameshRin@users.noreply.github.com>
2018-02-20 12:40:25 +0000

    更新cookies

--- 抢舰长台历.py
+++ 抢舰长台历.py
@@ -9,8 +9,7 @@ from email.mime.text import MIMEText
 URL = 'http://api.live.bilibili.com/activity/v1/NewSpring/redBagExchange'
 
 HEADER = {
-    'Cookie': 'l=v; finger=888236dc; LIVE_BUVID=AUTO7015179513350665; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1517951332; Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1517954404; sid=5tk0kq4c; fts=1517951337; buvid3=FF54B876-8A47-4AB1-8653-D64AB9F6549B14056infoc; DedeUserID=2892086; DedeUserID__ckMd5=dd5ed1395cc7afcd; SESSDATA=bfa227a2%2C1520543427%2C97501eda; bili_jct=0ccdc271ddb227fdc184a30bc2409258; _dfcaptcha=0a41f041c15d87059090c345612287cd',
-    #'Cookie': 'bili_jct=c29d3f8ddab5fb2fbbec2b1b5ec152c5; DedeUserID=56373701; DedeUserID__ckMd5=16f7317c14fdeac1; SESSDATA=42646427%2C1519386700%2C594fd78e; JSESSIONID=931E89C32EE07AED355B617FE5A26999; LIVE_BUVID=3e4530bbb6f7d5ddbf2bf182ce6c4fcf; LIVE_BUVID__ckMd5=557541bbbc2d8f0c; sid=k1ffv3oq'
+    'Cookie':'fts=1508263600; LIVE_BUVID=fe19b73c4d343def4790ad24d7900d82; LIVE_BUVID__ckMd5=06085ba75ced9876; rpdid=ipmliowwwpdoswqixkmpw; pgv_pvi=107897856; sid=law2ttpe; UM_distinctid=160c6868103690-000bd190b4e101-3c604504-1fa400-160c68681042c7; im_notify_type_2892086=0; im_local_unread_2892086=0; im_seqno_2892086=37692; finger=edc6ecda; DedeUserID=2892086; DedeUserID__ckMd5=dd5ed1395cc7afcd; SESSDATA=bfa227a2%2C1520329479%2Cfaf684e1; bili_jct=e44f6191d73d28475a8d81646db76c8a; balh_flv_prefer_ws=; balh_server=https://biliplus.ipcjs.win; BANGUMI_SS_21466_REC=173253; buvid3=4F9CDF71-D9F6-4208-BBED-B0EC0B68347E6793infoc; BANGUMI_SS_21680_REC=183804; BANGUMI_SS_21769_REC=173344; BANGUMI_SS_21755_REC=173173; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1518966149,1519057645,1519067570,1519082276; pgv_si=s1823692800; _dfcaptcha=25164cf960d6de225fcbc0335bcc44cf; Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1519126099'
     }
 
 PAYLOAD = {
@@ -40,21 +39,19 @@ ROUND = requests.get('http://api.live.bilibili.com/activity/v1/NewSpring/redBagP
 while True: 
 
     tmp = requests.get('http://api.live.bilibili.com/activity/v1/NewSpring/redBagPool').json().get('data')
-    pool = tmp.get('pool_list')
     counter = tmp.get('round')
-    #print 1
 
     if counter != ROUND:
 
-        '''
         r = requests.post(URL, headers = HEADER, data = PAYLOAD)
+        print r.content
 
         if r.json().get('code') == 0:
-            tmp = 'Mission Complete at: ' + str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min) + '\n\n' + r.text 
-            sendEmail('Success!',tmp,'582981961@qq.com')
-        '''
+            tmp = '完成时间: ' + str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min) + '\n\n' + r.text 
+            sendEmail('抢到了',tmp,'582981961@qq.com')
+
         sendEmail('奖池更新','第 ' + str(counter) + ' 池' + '\n' + '时间: ' + str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min),'582981961@qq.com')
 
         ROUND = counter
             
-    #time.sleep(0.5)
+    time.sleep(0.5)
